982ff5cbec5d545180d1c9ed00173c7832c499bd
hamink <hamin@atomrigs.io>
2022-12-22 17:32:21 -0700

    update next/image

--- components/SectionEven.js
+++ components/SectionEven.js
@@ -1,25 +1,34 @@
 import sectionStyles from '../styles/SectionEven.module.css';
-import Image from "next/legacy/image";
+import Image from "next/image";
 
 const SectionEven = ({title, desc, image}) => {
   return (
-    <div className={sectionStyles.container}>
-        <div className={sectionStyles.content}>
-            <div className={sectionStyles.imageArea}>
-                <Image src={image} alt="section-image" width='100%' height='100%' layout="responsive" objectFit="contain" />
-            </div> 
-            <div className={sectionStyles.textArea}>
-                <h2 className={sectionStyles.title}>
-                {title}
-                </h2>
-                <div className={sectionStyles.desc}>
-                {desc}
-                </div>
-            </div>
-        </div>
-    </div>
-  
-  )
+      <div className={sectionStyles.container}>
+          <div className={sectionStyles.content}>
+              <div className={sectionStyles.imageArea}>
+                  <Image
+                      src={image}
+                      alt="section-image"
+                      width='100%'
+                      height='100%'
+                      sizes="100vw"
+                      style={{
+                          width: "100%",
+                          height: "auto",
+                          objectFit: "contain"
+                      }} />
+              </div> 
+              <div className={sectionStyles.textArea}>
+                  <h2 className={sectionStyles.title}>
+                  {title}
+                  </h2>
+                  <div className={sectionStyles.desc}>
+                  {desc}
+                  </div>
+              </div>
+          </div>
+      </div>
+  );
 }
 
 export default SectionEven
