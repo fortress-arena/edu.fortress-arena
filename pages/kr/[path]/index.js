88484a2f194cae6c179be5ed6ec969c56de2ab34
hamink <hamin@atomrigs.io>
2022-12-26 20:58:07 -0700

    update config

--- pages/kr/[path]/index.js
+++ pages/kr/[path]/index.js
@@ -16,10 +16,20 @@ const koreanArticle = ({ article, titles, paths }) => {
 }
 
 export const getStaticProps = async (context) => {
-  const res = await fetch(`${server}/api/articles/kr/${context.params.path}`)
+  const res = await fetch(`${server}/api/articles/kr/${context.params.path}`, {
+    headers: {
+      Accept: 'application/json, text/plain, */*',
+      'User-Agent': '*',
+    },
+  })
   const article = await res.json()
 
-  const res_2 = await fetch(`${server}/api/articles/kr`)
+  const res_2 = await fetch(`${server}/api/articles/kr`, {
+    headers: {
+      Accept: 'application/json, text/plain, */*',
+      'User-Agent': '*',
+    },
+  })
   const articles = await res_2.json()
 
   const titles = articles.map(t => t.title)
@@ -36,7 +46,12 @@ export const getStaticProps = async (context) => {
 
   
 export const getStaticPaths = async () => {
-  const res = await fetch(`${server}/api/articles/kr`)
+  const res = await fetch(`${server}/api/articles/kr`, {
+    headers: {
+      Accept: 'application/json, text/plain, */*',
+      'User-Agent': '*',
+    },
+  })
   const articles = await res.json()
   const urls = articles.map((article) => article.path)
   const paths = urls.map((path) => ({ params: { path: path.toString() } }))
