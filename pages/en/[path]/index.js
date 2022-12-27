9fa9bc2a620e35b114691d36ac1a54585352796e
hamink <hamin@atomrigs.io>
2022-12-26 20:20:34 -0700

    start en content

--- pages/en/[path]/index.js
+++ pages/en/[path]/index.js
@@ -1,21 +1,35 @@
 import { server } from "../../../config";
+import Content from '../../../components/Content'
+import Footer from "../../../components/footer";
+import Nav from "../../../components/Nav";
 
-const enlishArticle = ({ article }) => {
+const enlishArticle = ({ article, titles, paths }) => {
     return (
         <>
-         <h1>{article.title}</h1>
+         <div style={{backgroundColor: article.background_color}}>
+          <Nav article={article} />
+          <Content article={article} />
+          <Footer titles={titles} paths={paths} />
+         </div>
         </>
     )
 }
 
 export const getStaticProps = async (context) => {
     const res = await fetch(`${server}/api/articles/en/${context.params.path}`)
-  
     const article = await res.json()
+
+    const res_2 = await fetch(`${server}/api/articles/en`)
+    const articles = await res_2.json()
+
+    const titles = articles.map(t => t.title)
+    const paths = articles.map(p => p.path)
   
     return {
       props: {
         article,
+        titles,
+        paths,
       },
     }
   }
